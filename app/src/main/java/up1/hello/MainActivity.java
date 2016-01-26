c76b98e49b19143a2def8a9ea36d8c576cefebcb
somkiat <somkiat.p@gmail.com>
2016-01-26 15:17:34 +0700

    Completed first task to add two number

--- app/src/main/java/up1/hello/MainActivity.java
+++ app/src/main/java/up1/hello/MainActivity.java
@@ -2,12 +2,46 @@ package up1.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    EditText resultEditText;
+    EditText firstOperandEditText;
+    EditText secondOperandEditText;
+    Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultEditText = (EditText) findViewById(R.id.result);
+        firstOperandEditText = (EditText) findViewById(R.id.firstOperand);
+        secondOperandEditText = (EditText) findViewById(R.id.secondOperand);
+        Button calculateButton = (Button) findViewById(R.id.calculate);
+        calculateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int first = 0;
+        try {
+            first = Integer.parseInt(firstOperandEditText.getText().toString());
+        } catch (NumberFormatException e) {
+        }
+
+        int second = 0;
+        try {
+            second = Integer.parseInt(secondOperandEditText.getText().toString());
+        } catch (NumberFormatException e) {
+        }
+
+        int result = first + second;
+
+        resultEditText.setText(String.valueOf(result));
     }
 }
