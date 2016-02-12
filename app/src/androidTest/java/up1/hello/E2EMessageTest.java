f6d6adf57272bbab1e91b490ec7e25d4b28b41c6
somkiat <somkiat.p@gmail.com>
2016-02-12 19:55:23 +0700

    Demo with Fastlane Screengrab

--- app/src/androidTest/java/up1/hello/E2EMessageTest.java
+++ app/src/androidTest/java/up1/hello/E2EMessageTest.java
@@ -3,9 +3,13 @@ package up1.hello;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -18,25 +22,32 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class E2EMessageTest {
 
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+
     @Rule
     public ActivityTestRule<MainActivity> mMainActivityTestRule
             = new ActivityTestRule<>(MainActivity.class);
 
-
     @Test
     public void show_message() {
         //Arrange
+        Screengrab.screenshot("show_message_01");
         onView(withId(R.id.firstOperand)).perform(typeText("5"));
         onView(withId(R.id.secondOperand)).perform(typeText("5"));
+        Screengrab.screenshot("show_message_02");
         onView(withId(R.id.calculate)).perform(click());
-
+        Screengrab.screenshot("show_message_03");
         onView(withId(R.id.message)).perform(typeText("Hello world"), closeSoftKeyboard());
-
+        Screengrab.screenshot("show_message_04");
         //Act
         onView(withId(R.id.back)).perform(click());
+        Screengrab.screenshot("show_message_05");
 
         //Assert
         onView(withId(R.id.message)).check(matches(withText("Hello world")));
+        Screengrab.screenshot("show_message_06");
     }
 
 }
